4ac1cb033468f72142e7223369f9c4e1c0ba23fd
Wojtek Oledzki <wojciech.oledzki@bskyb.com>
2012-06-08 00:40:06 +0100

    updating google analytics comp.

--- templates/components/modules/google/analytics/Component.php
+++ templates/components/modules/google/analytics/Component.php
@@ -1,8 +1,25 @@
 <?php
 namespace components\modules\google\analytics;
 
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
 class Component extends \Hoborg\Bundle\DisplayServiceBundle\Component\Component {
 
-	public function afterLoad() {
+	public function registerEventListeners(EventDispatcher $eventDispatcher) {
+		//$eventDispatcher->
+	}
+
+	public function render() {
+		$enabled = $this->data->getElementsByTagName('enabled');
+		if ($enabled->length < 0) {
+			return parent::render();
+		}
+
+		if ('dev' == H_DS_ENV || 'test' == H_DS_ENV) {
+			$enabled = $enabled->item(0);
+			$enabled->nodeValue = 0;
+		}
+
+		return parent::render();
 	}
 }
