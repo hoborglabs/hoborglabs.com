ebbfa2fb01e63ce5bdb2b620103270ae1f0aaf47
Wojtek Oledzki <wojciech.oledzki@bskyb.com>
2013-01-26 16:14:23 +0000

    [refs #4] initial draft for jmeter + beanshel

--- templates/components/modules/paragraph/Component.php
+++ templates/components/modules/paragraph/Component.php
@@ -13,12 +13,26 @@ class Paragraph extends Component {
 		parent::prepareData();
 		foreach ($this->data->documentElement->childNodes as $component) {
 			if ('markdown' === $component->nodeName) {
-				$mdFile = $component->getAttribute('file');
+				if ($component->hasAttribute('file')) {
+					$mdFile = $this->provider->getRootDir()
+							. '/' . $this->provider->getSitePrefix()
+							. '/' . $component->getAttribute('file');
+					$md = file_get_contents($mdFile);
+				} else {
+					$md = $component->nodeValue;
+				}
+				$html = Markdown($md);
+
 				$textNode = $this->data->importNode(
-						new DOMElement('text', Markdown(file_get_contents($mdFile))), true);
-				$this->data->documentElement->appendChild($textNode);
+						new DOMElement('text', $html), true);
+				$component->parentNode->replaceChild($textNode, $component);
 			}
 		}
 		unset($component);
+	}
+
+	protected function replaceImages($html) {
+		// '<h:img[^/]/>'
+		return $html;
 	}
 }
